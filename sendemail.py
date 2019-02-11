ba3f87b3af96ee48c9cb037066943c707d5189b0
hug <nedu_brb@hotmail.com>
2019-02-11 13:37:22 +0100

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -7,8 +7,8 @@ from email.mime.text import MIMEText
 
 subject = "An email with attachment from Python"
 body = "This is an email with attachment sent from Python"
-sender_email = "my@gmail.com"
-receiver_email = "your@gmail.com"
+sender_email = "nedu_brb@hotmail.com"
+receiver_email = "nedu_brb@hotmail.com"
 password = input("Type your password and press enter:")
 
 # Create a multipart message and set headers
@@ -21,7 +21,7 @@ message["Bcc"] = receiver_email  # Recommended for mass emails
 # Add body to email
 message.attach(MIMEText(body, "plain"))
 
-filename = "document.pdf"  # In same directory as script
+filename = "/home/bai/Downloads/123.pdf"  # In same directory as script
 
 # Open PDF file in binary mode
 with open(filename, "rb") as attachment:
